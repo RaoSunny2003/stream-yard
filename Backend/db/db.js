dba1d70a5043407be8313a335c617ccfe40e4198
Rao Sunny <suaveraosunny@gamil.com>
2024-09-06 23:59:31 +0530

    Some Blunders

---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  // Schema definition here
+  username: String,
+   password: String,
+   purchasedCourses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course'
+   }]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+  User,
+};
